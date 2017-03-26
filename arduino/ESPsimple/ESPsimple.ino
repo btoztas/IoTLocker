e85eeaf9aec44b656c525dd2a5267b47d8608ac8
Bruno Gonçalves <btoztas@gmail.com>
2017-03-26 12:21:44 +0100

    Implemented some arduino functions

---
+++ arduino/ESPsimple/ESPsimple.ino
@@ -0,0 +1,135 @@
+/* 
+ *  This test will print to a PHP script the content on String say
+ *  The URL for the PHP script is: http://web.ist.utl.pt/~ist179069/RMSF/get.php
+ *  You can see the result on: http://web.ist.utl.pt/~ist179069/RMSF/left.html
+ *  
+ *  The sequence of AT commands is the following:
+ *  
+ *  AT+CWJAP_CUR="GONCALVES","abrunheira123"
+ *  AT+CIPSTART="TCP","web.ist.utl.pt",80
+ *  AT+CIPSEND=170
+ *  POST /ist179069/RMSF/get.php HTTP/1.1
+ *  Host: web.ist.utl.pt
+ *  User-Agent: ESP8266
+ *  Content-Length: 15
+ *  Content-Type: application/x-www-form-urlencoded
+ *  
+ *  word=IMACOMMENT
+ */
+
+
+const int CH_PD = 5;
+
+void send_ESP(String command) {
+  
+  //Serial.print("\r\n\n******* ENVIADO *******\r\n"+command+"\r\n***********************\r\n\n");
+  
+  char *command_2 = command.c_str();
+  for(int i=0; i<strlen(command_2); i++){
+    Serial.print(command_2[i]);
+  }
+  //Serial.print(command);
+  Serial.print("\r\n");
+  delay(500);
+  
+}
+
+void connect_AP(String ap, String password){
+  
+  String command = "AT+CWJAP_CUR=\""+ap+"\",\""+password+"\"";
+  send_ESP(command);
+  delay(10000);
+
+}
+
+void disconnect_AP(){
+
+  String command = "AT+CWQAP";
+  send_ESP(command);
+  delay(5000);
+
+}
+
+void tcp_CONNECT(String serv, int port){
+
+  String command = "AT+CIPSTART=\"TCP\",\""+serv+"\","+port;
+  send_ESP(command);
+  delay(2500);
+
+}
+
+void tcp_DISCONNECT(){
+
+  String command = "AT+CIPCLOSE";
+  send_ESP(command);
+  delay(2500);
+
+}
+
+void tcp_POST(String message, String dir, String host){
+
+  String total = "POST "+dir+" HTTP/1.1\r\nHost: "+host+"\r\nUser-Agent: ESP8266\r\nContent-Length: "+(String)message.length()+"\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n"+message+"\r\n";
+  String post[6];
+  post[0] = "POST "+dir+" HTTP/1.1";
+  post[1] = "Host: "+host;
+  post[2] = "User-Agent: ESP8266";
+  post[3] = "Content-Length: "+(String)message.length();
+  post[4] = "Content-Type: application/x-www-form-urlencoded\r\n";
+  post[5] = message + "\r\n";
+  String command = "AT+CIPSEND="+(String)total.length();
+  send_ESP(command);
+  for(int i=0; i<6; i++){
+    send_ESP(post[i]);
+  }
+  
+  delay(2500);
+}
+
+void tcp_GET(String url, String host){
+  
+  String total = "GET "+url+" HTTP/1.1\r\nHost: "+host+"\r\nUser-Agent: ESP8266\r\n\r\n";
+  String gte[3];
+  gte[0] = "GET "+url+" HTTP/1.1";
+  gte[1] = "Host: "+host;
+  gte[2] = "User-Agent: ESP8266\r\n";
+  
+  String command = "AT+CIPSEND="+(String)total.length();
+  send_ESP(command);
+  for(int i=0; i<3; i++){
+    send_ESP(gte[i]);
+  }
+  delay(2500);
+  
+  
+}
+
+void setup() {
+
+
+  pinMode(CH_PD, OUTPUT); 
+  digitalWrite(CH_PD, LOW);
+  delay(1000);
+  digitalWrite(CH_PD, HIGH);
+  delay(2500);
+  Serial.begin(9600);
+
+
+  connect_AP("GONCALVES", "abrunheira123");
+
+  tcp_CONNECT("web.tecnico.ulisboa.pt", 80);
+
+  tcp_POST("word=<h1>BENFICA</h1>", "/ist179069/RMSF/get.php", "web.tecnico.ulisboa.pt");
+
+  tcp_GET("/ist179069/RMSF/index.html", "web.tecnico.ulisboa.pt");
+  
+  tcp_DISCONNECT();
+
+  disconnect_AP();
+  
+
+
+}
+
+void loop() {
+
+}
