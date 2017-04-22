22def5cbb75c29e941d6fafee84641c6cb330cd9
Bruno Gonçalves <btoztas@gmail.com>
2017-04-22 15:30:03 +0100

    TODO: FCM

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,13 +1,15 @@
 DROP TABLE IF EXISTS checkin;
 DROP TABLE IF EXISTS user;
-
+DROP TABLE IF EXISTS mobile;
+DROP TABLE IF EXISTS alert;
 
 CREATE TABLE user(
   id varchar(255),
   name varchar(255),
+  regday DATE,
+  reghour TIME,
   PRIMARY KEY (id)
 );
-
 
 CREATE TABLE checkin(
   id varchar(255),
@@ -15,4 +17,19 @@ CREATE TABLE checkin(
   hour TIME,
   PRIMARY KEY(id, day, hour),
   FOREIGN KEY (id) REFERENCES user (id)
+);
+
+CREATE TABLE alert(
+  id INTEGER,
+  day DATE,
+  hour TIME,
+  description varchar(255),
+  PRIMARY KEY (id, day, hour)
+);
+
+CREATE TABLE mobile (
+  token varchar(255),
+  regday DATE,
+  reghour TIME,
+  PRIMARY KEY (Token)
 );
